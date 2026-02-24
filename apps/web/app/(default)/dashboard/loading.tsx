e7bc9e8892746278acd4ad422765527b3e3b472c
Blas Vernazza <blasvernazza123@gmail.com>
2026-02-24 11:10:59 -0300

    Initial setup and /web initial folders setup

---
+++ apps/web/app/(default)/dashboard/loading.tsx
@@ -0,0 +1,5 @@
+export default function DashboardLoadingPage() {
+    return (
+        <div></div>
+    );
+}
---
+++ apps/web/app/(default)/dashboard/settings/loading.tsx
@@ -0,0 +1,5 @@
+export default function SettingsLoadingPage() {
+    return (
+        <div></div>
+    );
+}
